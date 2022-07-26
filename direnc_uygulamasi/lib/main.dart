737fb5bdf656a1082f0ed1dbb8e7300d6ef75900
SuleAlbursa <sduman@albursa.com>
2022-07-26 17:29:44 +0300

    Tasarım çalışmalarına başlanıldı.

--- direnc_uygulamasi/lib/main.dart
+++ direnc_uygulamasi/lib/main.dart
@@ -1,11 +1,13 @@
-import 'package:direnc_uygulamasi/screens/uc_bant_hesapla.dart';
+import 'package:direnc_uygulamasi/screens/uc_bant_hesaplama.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
